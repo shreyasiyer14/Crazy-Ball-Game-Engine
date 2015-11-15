eb704607715990d530e3fb171094e22e3ac11151
djeof-1 <shreyasiyer14@gmail.com>
2015-11-15 21:36:23 +0530

    Second Commit

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ y_change_2=0
 fps = pygame.time.Clock()
 gameDisplay = pygame.display.set_mode((800,600))
 
-pygame.display.set_caption('CraZzY Ball')
+pygame.display.set_caption('CraZY Ball')
 gameExit = False
 slider = pygame.image.load('Sprites/slider.bmp')
 slider2 = pygame.transform.rotate(slider, -90)
@@ -56,57 +56,53 @@ while not gameExit:
               			x_change_2 = 0
                			y_change_2 = 0
 	
-	ball_x+=dx
-	ball_y+=dy
-
-	pygame.draw.rect(gameDisplay, black, (20,20,50,50))
+	
 	cur_x_1+=x_change_1
 	cur_y_1+=y_change_1
    	cur_x_2+=x_change_2
 	cur_y_2+=y_change_2
 	
-	if dx>=0 and ball_x>=cur_x_1 and ball_x<=cur_x_1+40 and ball_y==cur_y_1+40:
-		dx*=-1
+	ball_x+=dx
+	ball_y+=dy
+	if dx>=0 and ball_x>=cur_x_1 and ball_x<=cur_x_1+40 and ball_y==cur_y_1:
+		dx*=1
 		dy*=-1
-
-	elif dx<=0 and ball_x<=cur_x_1+80 and ball_x>=cur_x_1+40 and ball_y==cur_y_1+40:
-		dx*=-1
+	elif dx<=0 and ball_x<=cur_x_1+80 and ball_x>=cur_x_1+40 and ball_y==cur_y_1:
+		dx*=1
 		dy*=-1
-
 	elif dx>=0 and ball_x<=cur_x_1+80 and ball_x>=cur_x_1+40 and ball_y==cur_y_1:
 		dx*=1
 		dy*=-1
-
 	elif dx<=0 and ball_x<=cur_x_1+40 and ball_x>=cur_x_1 and ball_y==cur_y_1:
 		dx*=1
 		dy*=-1
-
-
 	if dy>=0 and ball_x==50 and ball_y<=cur_y_2+40 and ball_y>=cur_y_2:
 		dx*=-1
-		dy*=-1
-
+		dy*=1
 	elif dy<0 and ball_x==50 and ball_y>=cur_y_2+40 and ball_y<=cur_y_2+80:
 		dx*=-1
 		dy*=1
-
 	elif dy<0 and ball_x==50 and ball_y>=cur_y_2+40 and ball_y<=cur_y_2+80:
 		dx*=-1
-		dy*=1
-	
+		dy*=1	
 	elif dy>=0 and ball_x==50 and ball_y>=cur_y_2+40 and ball_y<=cur_y_2+80:
 		dx*=-1
 		dy*=1
-
-	if ball_x<=0 or ball_x>=790:
+	if ball_x<=0: 
+		gameExit = True
+		print "You lose"
+	elif ball_x>=790:
 		dx*=-1
-	if ball_y<=0 or ball_y>=590:
+	if ball_y>=590:
+		gameExit = True
+		print "You lose"
+	elif ball_y<0:
 		dy*=-1
 
 	gameDisplay.fill(white)
 	gameDisplay.blit(slider, (cur_x_1, cur_y_1))
 	gameDisplay.blit(slider2, (cur_x_2, cur_y_2))
-	pygame.draw.circle(gameDisplay, (100,90,90), (ball_x,ball_y),10,0)
+	pygame.draw.circle(gameDisplay, (100,90,90),(ball_x,ball_y),10,0)
 	pygame.display.update()
 	fps.tick(30)
 pygame.quit()
