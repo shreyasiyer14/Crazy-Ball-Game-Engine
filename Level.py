17f1eb650c64133a7fb9549346a77e07380f7a6b
djeof-1 <shreyasiyer14@gmail.com>
2015-12-07 22:24:43 +0530

    A simple “Breakout” kind of arcade game Engine, built using Python and Pygame

--- Level.py
+++ Level.py
@@ -4,6 +4,10 @@ class Level:
     def __init__(self,num):
         self.levelNum = num
 
+    #A function to return a list of 2D matrices, containing 0s and 1s. 
+    #When certain index is 1, then it tells the renderer to make a brick there, else dont make it.
+
+   
     def levels(self):
         level = []
         level.append([[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
--- main.py
+++ main.py
@@ -14,25 +14,35 @@ red=(255,0,0)
 green=(0,155,0)
 yellow=(200,200,0)
 
+#Import Comic Sans Font for using in displaying the score.
 font=pygame.font.SysFont("comicsansms", 20)
+
+#Setting the frames per second, the frequency of refreshing of the screen.
 fps = pygame.time.Clock()
+
+#Setting up the main window.
 gameDisplay = pygame.display.set_mode((800,600))
 
 pygame.display.set_caption('Crazy Ball')
 
+#Import the image files to be used as sliders.
 icon = pygame.image.load('Sprites/slider.bmp')
 pygame.display.set_icon(icon)
 
 smallfont=pygame.font.SysFont("courier", 25)
 medfont=pygame.font.SysFont("courier", 50)
 largefont=pygame.font.SysFont("courier", 80)
-pygame.mixer.init(frequency=44100, size=-16,channels= 2, buffer=4096)
-pygame.mixer.music.load('Sounds/BeatWave.mp3')
 
+#Load music files.
+#pygame.mixer.init(frequency=44100, size=-16,channels= 2, buffer=4096)
+#pygame.mixer.music.load('Sounds/BeatWave.mp3')
+
+#Multithreading music sounds, to play continuously in the background.
 def gameSound():
 	pygame.mixer.music.load('Sounds/Seasons.mp3')
 	pygame.mixer.music.play(-1)
 
+#Function to initialize text objects of different sizes.
 def text_objects(text,color,size):
 	if size == "small":
 		textSurface = smallfont.render(text,True,color)
@@ -48,15 +58,18 @@ def text_to_button(msg,color,buttonx,buttony,buttonwidth,buttonheight,size="smal
 	textRect.center = (buttonx+(buttonwidth/2)),(buttony+(buttonheight/2))
 	gameDisplay.blit(textSurf,textRect)
 
+#Blitting message to the screen, such as "Game over" or "You Win!".
 def message_to_screen(msg,color,y_displace=0,size = "small"):
 	textSurf,textRect = text_objects(msg,color,size)
 	textRect.center =(800/2),(600/2)+y_displace
 	gameDisplay.blit(textSurf,textRect)
 
+#Displaying the score.
 def score(count):
     scores = font.render("Score: "+str(count),1,(255,255,255))
     gameDisplay.blit(scores,(20,20))
 
+#Starting screen.
 def game_info():
 	info = True
 	while info:
@@ -107,8 +120,11 @@ def button(text,x,y,width,height,inactive_color,active_color,action=None):
 	text_to_button(text,white,x,y,width,height)
 
 randcirclelist = []
+
+#Start Screen.
 def game_intro():
-    intro = True 
+    intro = True
+    #Two balls with random coordinates, for display. 
     ball_x=random.randrange(5,500)
     ball_y=random.randrange(5,500)
 
@@ -119,9 +135,10 @@ def game_intro():
 
     dx2=random.randrange(-10,10)
     dy2=random.randrange(1,10)
-    pygame.mixer.music.play(-1)
+#    pygame.mixer.music.play(-1)
     
     while intro:
+	#Initialize a Circle object, and append it to a list.
 	randcircle = IntroCircle(5)
 	randcirclelist.append(randcircle)
 	
@@ -141,6 +158,8 @@ def game_intro():
 	
         ball_x2+=dx2
 	ball_y2+=dy2
+
+	#The condition to change the direction of the balls when the ball hits the edges of the screen.
         if ball_x<=0:
         	dx*=-1
 	elif ball_x>=790:
@@ -175,6 +194,7 @@ def game_intro():
 	pygame.display.update()
 	fps.tick(20)
 
+#Pause the game.
 def pause():
 	paused = True
 	
@@ -195,7 +215,7 @@ def pause():
 					pygame.quit()
 					quit()
 		fps.tick(5)
-
+#Function that will be called when the player successfully completes the game.
 def levelcomplete(count):
     paused = True
     message_to_screen("Congratulations!",white,-100,size="large")
@@ -222,7 +242,7 @@ def levelcomplete(count):
 slider = pygame.image.load('Sprites/slider.bmp')
 slider2 = pygame.transform.rotate(slider, -90)
 
-
+#Function to detect the collision and return value whether the collision was horizontal or vertical.
 def detectCollisions(x1,y1,w1,h1,x2,y2,w2,h2):
            	if (x2+w2>=x1+w1>=x2 and y2+h2>=y1>=y2):
             	    return True,3
@@ -235,14 +255,16 @@ def detectCollisions(x1,y1,w1,h1,x2,y2,w2,h2):
 	    	else:
 		    return False,0 
 
+#Main game loop.
 def gameLoop():
+	#Score
         count = 0
         
 	randIndex = random.randrange(0,3)
         level_obj = Level(randIndex)
         level = level_obj.levels()
 	brickList = []
-
+	#Set up a color for each row of the bricks.
 	for y in range(len(level)):
 		color = (random.randrange(50,200),random.randrange(50,200),random.randrange(50,100))
     		for x in range(len(level[y])):
@@ -268,6 +290,7 @@ def gameLoop():
 	ball_x=random.randrange(250,400)
 	ball_y=random.randrange(250,500)
 	
+	#To initialize the sprite object.
 	spriteball = Sprite(ball_x, ball_y)
 	#dx=random.randrange(3,5)
 	#dy=random.randrange(3,5)
@@ -293,6 +316,7 @@ def gameLoop():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				gameExit = True
+		#Keyboard events.
 			if event.type == pygame.KEYDOWN:
 				if event.key == pygame.K_LEFT:
 					x_change_1=-10
@@ -332,7 +356,7 @@ def gameLoop():
 		if cur_y_2 <= 0:
 			cur_y_2 = 0
 			
-			
+		#Collision with the sliders.
 		a1,b1=detectCollisions(ball_x, ball_y, 10,10,cur_x_1, cur_y_1-12,80,10)
 		
 		if (ball_y + 10) > cur_y_1 and (ball_x+10 < cur_x_1 or ball_x > cur_x_1+80):
@@ -366,6 +390,7 @@ def gameLoop():
 	 	gameDisplay.blit(slider, (cur_x_1, cur_y_1))
 	   	gameDisplay.blit(slider2, (cur_x_2, cur_y_2))
 	   	for brick in brickList:
+		#If the ball collides with the brick, then remove the brick from brickList.
 		     a,b = detectCollisions(ball_x, ball_y, 10,10,brick.x, brick.y, brick.width, brick.height)
 	       	     if a == True and b == 3:
 			    brickList.remove(brick)
@@ -386,12 +411,15 @@ def gameLoop():
 			    #ball_x+=dx
 		ball_x+=dx
 		ball_y+=dy
+		#Update the x and y coordinates of the sprite.
 		spriteball.x=ball_x
 		spriteball.y=ball_y
 		spriteball.update(gameDisplay)
 		
 	        if len(brickList)==0:
         	    levelcomplete(count)
+
+		#Update the bricks and display the score accordingly.
 		for brick in brickList:
 	            brick.render(gameDisplay)
 	       	    score(count)
